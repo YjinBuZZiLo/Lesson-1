e937386ec203900cd4d031f55742c65cf233f47f
YjinBuZZiLo <evgene2305@gmail.com>
2024-09-14 17:56:29 +0300

    Add files via upload

---
+++ int.py
@@ -0,0 +1,58 @@
+
+### Задание 1
+#data_input = "Введите дату:"
+#data = "Сегодня"
+#print(data_input, data)
+#task_input = "Введите задачу:"
+#task = "Сделать все дела"
+#print(task_input, task)
+#print(data, task)
+
+
+
+
+### Задание 2
+# data_input = "Введите дату:"
+# task_input = "Введите задачу:"
+# data1 = "Сегодня"
+# print(data_input, data1)
+# task1 = "Выучить Python"
+# print(task_input, task1)
+# data2 = "Завтра"
+# print(data_input, data2)
+# task2 = "Разработать TODO-приложение"
+# print(task_input, task2)
+# data3 = "Послезавтра"
+# print(data_input, data3)
+# task3 = "Разработать Telegram-бота"
+# print(task_input, task3)
+# print(data1, task1)
+# print(data2, task2)
+# print(data3, task3)
+
+
+
+
+
+###Задание 3
+task_dict = {}
+
+data_input = "Введите дату:"
+task_input = "Введите задачу:"
+data1 = "Сегодня"
+task1 = "Выучить Python"
+data_dict1 = {data_input, data1}
+task_dict1 = {task_input, task1}
+data2 = "Завтра"
+task2 = "Разработать TODO-приложение"
+data_dict2 = {data_input, data2}
+task_dict2 = {task_input, task2}
+data3 = "Послезавтра"
+task3 = "Разработать Telegram-бота"
+data_dict3 = {data_input, data3}
+task_dict3 = {task_input, task3}
+
+
+
+
+
